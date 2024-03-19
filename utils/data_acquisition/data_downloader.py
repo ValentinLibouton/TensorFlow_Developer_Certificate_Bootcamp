2a839c1be01fd733a16e295f56c2c62c9bccf6fe
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2024-03-19 16:34:32 +0100

    add doc string in utils functions

--- utils/data_acquisition/data_downloader.py
+++ utils/data_acquisition/data_downloader.py
@@ -12,18 +12,45 @@ except ImportError:
     py7zr = None
 
 def extract_zip_file(filename, destination=None):
+    """
+    Extracts a ZIP file to a specified destination directory.
+
+    This function opens a ZIP file specified by 'filename' and extracts its contents to the directory specified by 'destination'. If 'destination' is not provided, the contents are extracted to the current working directory. The function prints messages indicating the start and completion of the extraction process.
+
+    Parameters:
+    - filename (str): The path to the ZIP file to be extracted.
+    - destination (str, optional): The directory to which the contents of the ZIP file should be extracted. Defaults to None, which results in extraction to the current working directory.
+    """
     print(f"Extracting {filename} as ZIP...")
     with zipfile.ZipFile(filename, 'r') as zip_ref:
         zip_ref.extractall(path=destination)
     print(f"{filename} has been extracted to {destination if destination else 'current directory'}.")
 
 def extract_tar_file(filename, destination=None):
+    """
+    Extracts a TAR.GZ file to a specified destination directory.
+
+    Opens a TAR.GZ file specified by 'filename' and extracts its contents to the directory specified by 'destination'. If 'destination' is not provided, the contents are extracted to the current working directory. Messages are printed to indicate the start and completion of the extraction process.
+
+    Parameters:
+    - filename (str): The path to the TAR.GZ file to be extracted.
+    - destination (str, optional): The directory where the contents should be extracted. Defaults to the current working directory.
+    """
     print(f"Extracting {filename} as TAR.GZ...")
     with tarfile.open(filename, 'r:gz') as tar_ref:
         tar_ref.extractall(path=destination)
     print(f"{filename} has been extracted to {destination if destination else 'current directory'}.")
 
 def extract_7z_file(filename, destination=None):
+    """
+    Extracts a 7z file to a specified destination directory, if the py7zr module is available.
+
+    Attempts to open a 7z file specified by 'filename' and extract its contents to 'destination'. If 'destination' is not provided, the contents are extracted to the current working directory. If the py7zr module is not installed, prints a message indicating that the module is required for extracting 7z files.
+
+    Parameters:
+    - filename (str): The path to the 7z file to be extracted.
+    - destination (str, optional): The directory where the contents should be extracted. Defaults to the current working directory.
+    """
     if py7zr:
         print(f"Extracting {filename} as 7z...")
         with py7zr.SevenZipFile(filename, mode='r') as zip_ref:
@@ -33,6 +60,15 @@ def extract_7z_file(filename, destination=None):
         print("py7zr module not available. Unable to extract .7z files.")
 
 def extract_archive_file(filename, destination=None):
+    """
+    Determines the type of an archive file using the magic module and extracts it to a specified destination.
+
+    Uses the magic module to identify the MIME type of 'filename' and calls the appropriate function to extract its contents. Supports ZIP, TAR.GZ, TAR, BZIP2, and 7Z formats. If the magic module is not available, prints an error message.
+
+    Parameters:
+    - filename (str): The path to the archive file to be extracted.
+    - destination (str, optional): The directory where the contents should be extracted. Defaults to the current working directory.
+    """
     if magic:
         mime_type = magic.from_file(filename, mime=True)
         if mime_type == 'application/zip':
@@ -53,6 +89,18 @@ def extract_archive_file(filename, destination=None):
 
 
 def download_data(url, filename, extract=False, destination=None, remove_archive=False):
+    """
+    Downloads a file from a specified URL and optionally extracts it.
+
+    Downloads a file from 'url' to 'filename'. If 'extract' is True, attempts to extract the file using 'extract_archive_file'. If 'remove_archive' is True, deletes the archive file after extraction. Prints messages to indicate the status of download and extraction.
+
+    Parameters:
+    - url (str): The URL from which to download the file.
+    - filename (str): The local filename to save the downloaded file.
+    - extract (bool, optional): Whether to extract the downloaded file. Defaults to False.
+    - destination (str, optional): The directory where the contents should be extracted. Defaults to the current working directory.
+    - remove_archive (bool, optional): Whether to remove the archive file after extraction. Defaults to False.
+    """
     if not os.path.exists(filename):
         # Download the file using wget
         subprocess.run(["wget", "-nc", url, "-O", filename])
